23d0af62f18c0b5653014e456ee7cc6e8d5442a9
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2022-12-03 23:03:38 -0500

    Day 1 and 2

---
+++ day1/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
+	file, err := os.Open("day1/input")
+	if err != nil {
+		log.Fatalf("Error reading file")
+	}
+	defer file.Close()
+
+	mostCalories := 0
+	currentElfCalories := 0
+	var currentLine string
+	var currentInt int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		currentLine = scanner.Text()
+		if currentLine == "" {
+			if currentElfCalories > mostCalories {
+				mostCalories = currentElfCalories
+			}
+			currentElfCalories = 0
+		} else {
+			currentInt, err = strconv.Atoi(currentLine)
+			if err != nil {
+				log.Fatalf("Bad string to int conversion")
+			}
+			currentElfCalories += currentInt
+		}
+	}
+
+	fmt.Printf("Day 1 - Part 1: %d\n", mostCalories)
+}
+
+func partTwo() {
+	file, err := os.Open("day1/input")
+	if err != nil {
+		log.Fatalf("Error reading file")
+	}
+	defer file.Close()
+
+	allCalories := make([]int, 0)
+	currentElfCalories := 0
+	var currentLine string
+	var currentInt int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		currentLine = scanner.Text()
+		if currentLine == "" {
+			allCalories = append(allCalories, currentElfCalories)
+			currentElfCalories = 0
+		} else {
+			currentInt, err = strconv.Atoi(currentLine)
+			if err != nil {
+				log.Fatalf("Bad string to int conversion")
+			}
+			currentElfCalories += currentInt
+		}
+	}
+
+	sort.Ints(allCalories)
+	allCaloriesLen := len(allCalories)
+	fmt.Printf("Day 1 - Part 2: %d\n", allCalories[allCaloriesLen-1]+allCalories[allCaloriesLen-2]+allCalories[allCaloriesLen-3])
+}
---
+++ day2/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	Rock     = 1
+	Paper    = 2
+	Scissors = 3
+)
+
+const (
+	Lose = 0
+	Tie  = 3
+	Win  = 6
+)
+
+func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
+	// FIRST COLUMN -> Opponent Play
+	// A = ROCK
+	// B = PAPER
+	// C = SCISSORS
+
+	// SECOND COLUMN -> Your Play
+	// X = ROCK
+	// Y = PAPER
+	// Z = SCISSORS
+
+	// Play points
+	// 1 = ROCK
+	// 2 = PAPER
+	// 3 = SCISSORS
+
+	// Outcome points
+	// 0 = Lose
+	// 3 = Tie
+	// 6 = Win
+
+	// Total score = sum of scores for each round
+	// Score per round = played points + outcome points
+
+	var playToType = map[uint8]int{
+		'A': Rock,
+		'B': Paper,
+		'C': Scissors,
+		'X': Rock,
+		'Y': Paper,
+		'Z': Scissors,
+	}
+
+	file, err := os.Open("day2/input")
+	if err != nil {
+		log.Fatalf("Error reading file")
+	}
+	defer file.Close()
+
+	var currentLine string
+	scanner := bufio.NewScanner(file)
+
+	totalScore := 0
+	for scanner.Scan() {
+		currentLine = scanner.Text()
+
+		opponentPlay := playToType[currentLine[0]]
+		yourPlay := playToType[currentLine[2]]
+
+		totalScore += yourPlay // Score the shape you selected
+
+		// Score outcome of match (do nothing for lose condition)
+		if opponentPlay == yourPlay {
+			totalScore += Tie
+		} else if yourPlay == Rock && opponentPlay == Scissors {
+			totalScore += Win
+		} else if yourPlay == Paper && opponentPlay == Rock {
+			totalScore += Win
+		} else if yourPlay == Scissors && opponentPlay == Paper {
+			totalScore += Win
+		}
+	}
+
+	fmt.Printf("Day 2 - Part 1: %d\n", totalScore)
+}
+
+func partTwo() {
+	var playOrOutcomeToType = map[uint8]int{
+		'A': Rock,
+		'B': Paper,
+		'C': Scissors,
+		'X': Lose,
+		'Y': Tie,
+		'Z': Win,
+	}
+
+	var whatShapeBeatsMe = map[int]int{
+		Rock:     Paper,
+		Paper:    Scissors,
+		Scissors: Rock,
+	}
+
+	var whatShapeLosesAgainstMe = map[int]int{
+		Rock:     Scissors,
+		Paper:    Rock,
+		Scissors: Paper,
+	}
+
+	file, err := os.Open("day2/input")
+	if err != nil {
+		log.Fatalf("Error reading file")
+	}
+	defer file.Close()
+
+	var currentLine string
+	scanner := bufio.NewScanner(file)
+
+	totalScore := 0
+	for scanner.Scan() {
+		currentLine = scanner.Text()
+
+		opponentPlay := playOrOutcomeToType[currentLine[0]]
+		yourOutcome := playOrOutcomeToType[currentLine[2]]
+
+		totalScore += yourOutcome // Score outcome
+
+		// Score shape you had to pick for desired outcome
+		if yourOutcome == Tie {
+			totalScore += opponentPlay
+		} else if yourOutcome == Win {
+			totalScore += whatShapeBeatsMe[opponentPlay]
+		} else if yourOutcome == Lose {
+			totalScore += whatShapeLosesAgainstMe[opponentPlay]
+		}
+	}
+
+	fmt.Printf("Day 2 - Part 2: %d\n", totalScore)
+}
